603902793676176a0829c409e3f24cdac630f2d4
letuu <albertvt@ya.ru>
2022-12-18 08:08:56 +0300

    Initial commit

---
+++ src/main/java/pro/sky/algorithmsarraylist/AlgorithmsArraylistApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.algorithmsarraylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsArraylistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsArraylistApplication.class, args);
+    }
+
+}
